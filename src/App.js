b277b55bed64894ca13418a21938cf970c7bdac9
Shreeti-Bajracharya <shreetibajracharya73@gmail.com>
2024-02-29 21:32:30 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import Homeapp from './component/Home/HomeApp';
+//import Reg from './component/prc/reg';
+//import Register from './component/register/register';
+import Adminapp from './component/practice/admin/adminapp';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <>
+    <Homeapp/>
+
+    </>
+  );
+}
+
+export default App;
